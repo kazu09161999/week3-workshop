ea2dc9fb3d4ac760d91c93dc68707ff06fa7f232
Kazuki Yoneyama <y.kazu_0916@icloud.com>
2022-08-06 21:16:05 +1000

    figure out why didn't work on website

--- project_server/server.js
+++ project_server/server.js
@@ -1,16 +1,37 @@
 var express = require('express'); //used for routing
 var app = express();
-var http = require('http').Server(app); //used to provide http functionality
+// var http = require('http').Server(app); //used to provide http functionality
+var bodyParser = require('body-parser');
+app.use (bodyParser.json()); 
 
 app.use(express.static(__dirname + '/www'));
 
-let server = http.listen(3000, function () {
-  let host = server.address().address;
-  let port = server.address().port;
-  console.log("My First Nodejs Server!");
-  console.log("Server listening on: "+ host + " port:" + port);
- });
+app.listen(3000, ()=>{
+  var d = new Date();
+  var n = d.getHours();
+  var m = d.getMinutes();
+  console.log('Server has been started at : ' + n + ':' + m);
+});
 
-app.get('/test', function (req, res) {
-  res.sendFile(__dirname + '/www/test.html');
-  });
+app.get('/', function(req, res){
+
+  res.sendFile(__dirname + '/www/index.html');
+});
+
+app.post('/api/login', function(req,res){
+  let users = [{'email':'abc@com.au', 'pwd':'123'},{'email':'abd@com.au', 'pwd':'123'},{'email':'abe@com.au', 'pwd':'123'} ]
+
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+  var customer = {};
+  customer.email = req.body.email;
+  customer.upwd = req.body.upwd;
+  customer.valid = false;
+for (let i=0;i<users.length;i++){
+  if (req.body.email == users[i].email && req.body.upwd == users[i].pwd) {
+    customer.valid = true;
+  }
+}
+  res.send(customer);
+});
